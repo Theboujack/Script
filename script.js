1ed710329f3ecec4b8f306d21b95311abeda9df6
Theboujack <159936097+Theboujack@users.noreply.github.com>
2024-10-22 19:08:26 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
-const playButton = document.getElementById('playButton');
+// const playButton = document.getElementById('playButton');
 
-playButton.addEventListener('click', function () {
+//playButton.addEventListener('click', function () {
   // Ganti 'new-page.html' dengan halaman tujuan Anda
-  window.location.href = 'new-page.html';
+//  window.location.href = '';
 });
